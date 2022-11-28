f4d92b26c4d66dc0d5b43b16bd4f5394c6bb51e6
Ida Danielsson <ida.danielsson96@icloud.com>
2022-11-28 14:22:11 +0100

    Added new Todos

---
+++ src/ts/main.ts
@@ -0,0 +1,9 @@
+import { Todo } from "./modules/list";
+
+let firstTodo = new Todo("Promenad");
+let secondTodo = new Todo("Städa");
+let thirdTodo = new Todo("Plugga");
+let fourthTodo = new Todo("Tvätta");
+let fifthTodo = new Todo("Gymma");
+
+let taskList = [firstTodo, secondTodo, thirdTodo, fourthTodo, fifthTodo];
